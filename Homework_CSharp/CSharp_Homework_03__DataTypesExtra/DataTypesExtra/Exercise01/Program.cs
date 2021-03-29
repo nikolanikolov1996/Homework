02933b1e31129070a0c79618da63c9d8bce9828a
Nikola Nikolov <nikola.nikolov96@outlook.com>
2021-03-29 15:49:53 +0200

    Added Homework DataTypesExtra

---
+++ Homework_CSharp/CSharp_Homework_03__DataTypesExtra/DataTypesExtra/Exercise01/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the temperature in Celsius expressed in numbers: ");
+            string tempInput = Console.ReadLine();
+            double temperature;
+
+            bool checkTempInput = double.TryParse(tempInput, out temperature);
+
+            if(checkTempInput)
+            {
+                if(temperature < 0)
+                {
+                    Console.WriteLine("It's freezing weather!");
+                }
+                else if(temperature >= 0 && temperature < 10)
+                {
+                    Console.WriteLine("It's very cold weather!");
+                }
+                else if(temperature >= 10 && temperature <20)
+                {
+                    Console.WriteLine("It's cold weather!");
+                }
+                else if (temperature >= 20 && temperature < 30)
+                {
+                    Console.WriteLine("It's normal in Temp!");
+                }
+                else if (temperature >= 30 && temperature < 40)
+                {
+                    Console.WriteLine("It's hot weather!");
+                }
+                else if (temperature >= 40)
+                {
+                    Console.WriteLine("It's very hot weather!");
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong. Please try again!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You must enter celsius expressed in numbers in order to continue!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework_CSharp/CSharp_Homework_03__DataTypesExtra/DataTypesExtra/Exercise02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the first number: ");
+            string firstNumInput = Console.ReadLine();
+            int firstNum;
+
+            Console.Write("Please enter the second number: ");
+            string secondNumInput = Console.ReadLine();
+            int secondNum;
+
+            bool checkFirstInput = int.TryParse(firstNumInput, out firstNum);
+            bool checkSecondInput = int.TryParse(secondNumInput, out secondNum);
+
+            int sumOfTwo;
+
+            if (checkFirstInput && checkSecondInput)
+            {
+
+                if (firstNum == secondNum)
+                {
+                    sumOfTwo = firstNum + secondNum;
+                    int sumMultiplied3 = sumOfTwo * 3;
+                    Console.WriteLine(sumMultiplied3);
+                }
+                else
+                {
+                    sumOfTwo = firstNum + secondNum;
+                    Console.WriteLine(sumOfTwo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter numbers in order to compute their sum!!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
